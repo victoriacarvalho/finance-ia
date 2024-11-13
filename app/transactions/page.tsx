066de3ef1bd56ba27d3dac9552b5968c3b43431f
Victoria Carvalho <you@example.com>
2024-11-13 15:39:23 -0300

    feat: transactions and subscription pages are protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,9 +3,20 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_colums";
 import AddTransactionButton from "../_components/add-transaction-buton";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
-  const transactions = await db.transaction.findMany({});
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <>
       <Navbar />
