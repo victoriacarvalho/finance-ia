4ebbdbf73f30f6f789502c0bfa36f21c6d62cb1c
Victoria Carvalho <65203217+victoriacarvalho@users.noreply.github.com>
2024-11-26 11:56:11 -0300

    Update summary-cards.tsx

--- app/(home)/_components/summary-cards.tsx
+++ app/(home)/_components/summary-cards.tsx
@@ -22,8 +22,7 @@ const SummaryCards = async ({
   expensesTotal,
 }: SummaryCards) => {
   return (
-    <div className="space-y-6">
-      {/* Saldo Card */}
+    <div className="space-y-6 ">
       <SummaryCard
         icon={<WalletIcon size={16} />}
         title="Saldo"
@@ -31,8 +30,7 @@ const SummaryCards = async ({
         size="large"
       />
 
-      {/* Cards de Investido, Receita e Despesa */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-3 gap-6">
         <SummaryCard
           icon={<PiggyBankIcon size={16} />}
           title="Investido"
