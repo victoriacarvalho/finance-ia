4975bb48b15c7a7832df9cd4a5ebcf3eb6248b5c
Victoria Carvalho <65203217+victoriacarvalho@users.noreply.github.com>
2024-11-26 13:29:58 -0300

    Update schema.ts

--- app/(home)/_actions/generate-ai-report/schema.ts
+++ app/(home)/_actions/generate-ai-report/schema.ts
@@ -1,13 +1,8 @@
+import { isMatch } from "date-fns";
 import { z } from "zod";
 
-// Expressão regular para garantir que o mês esteja no formato "MM" (01 a 12)
-const monthRegex = /^(0[1-9]|1[0-2])$/;
-
 export const generateAiReportSchema = z.object({
-  month: z.string().refine((value) => monthRegex.test(value), {
-    message: "O mês deve estar no formato MM (01 a 12)",
-  }),
+  month: z.string().refine((value) => isMatch(value, "MM")),
 });
 
 export type GenerateAiReportSchema = z.infer<typeof generateAiReportSchema>;
-
