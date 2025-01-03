c010e0f0e33e17bddb2700d195c5a58d89c8d0ba
Victoria Carvalho <65203217+victoriacarvalho@users.noreply.github.com>
2025-01-03 15:15:51 -0300

    Update page.tsx

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -5,7 +5,7 @@ import AddTransactionButton from "../_components/add-transaction-buton";
 import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import TimeSelect from "./_components/time-select";
+import TimeSelect from "../home/_components/time-select";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
