1cf621be1945f93fcc38f707ad44454fbaf7ab21
Victoria Carvalho <65203217+victoriacarvalho@users.noreply.github.com>
2025-04-02 10:36:38 -0300

    feat: rename months.tsx

--- app/(home)/_components/time-select.tsx
+++ app/(home)/_components/time-select.tsx
@@ -4,18 +4,18 @@ import { SelectTrigger, SelectValue } from "@radix-ui/react-select";
 import { useRouter, useSearchParams } from "next/navigation";
 
 const MONTH_OPTIONS = [
-    { value: "01", label: 'January' },
-    { value: "02", label: 'February' },
-    { value: "03", label: 'March' },
-    { value: "04", label: 'April' },
-    { value: "05", label: 'May' },
-    { value: "06", label: 'June' },
-    { value: "07", label: 'July' },
-    { value: "08", label: 'August' },
-    { value: "09", label: 'September' },
-    { value: "10", label:'October' },
-    { value: "11", label:'November' },
-    { value: "12", label: 'December' },
+    { value: "01", label: 'Janeiro' },
+    { value: "02", label: 'Fevereiro' },
+    { value: "03", label: 'Março' },
+    { value: "04", label: 'Abril' },
+    { value: "05", label: 'Maio' },
+    { value: "06", label: 'Junho' },
+    { value: "07", label: 'Julho' },
+    { value: "08", label: 'Agosto' },
+    { value: "09", label: 'Setembro' },
+    { value: "10", label:'Outubro' },
+    { value: "11", label:'Novembro' },
+    { value: "12", label: 'Dezembro' },
   ];
 const TimeSelect = () => {
     const { push } = useRouter();
