c494486150950daeb8b71e1e5b14f7273dc463e1
Victoria Carvalho <65203217+victoriacarvalho@users.noreply.github.com>
2025-04-02 10:14:27 -0300

    fix: Delete app/(home)/_actions/generate-ai-report directory

--- app/(home)/_actions/generate-ai-report/schema.ts
+++
@@ -1,8 +0,0 @@
-import { isMatch } from "date-fns";
-import { z } from "zod";
-
-export const generateAiReportSchema = z.object({
-  month: z.string().refine((value) => isMatch(value, "MM")),
-});
-
-export type GenerateAiReportSchema = z.infer<typeof generateAiReportSchema>;
